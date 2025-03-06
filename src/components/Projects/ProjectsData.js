4b1bc4ac26d7456af824769e963423b705d9804a
adriannylelis <adrianny.lelis@gmail.com>
2025-03-06 01:03:35 -0400

    update projetcts data

--- src/components/Projects/ProjectsData.js
+++ src/components/Projects/ProjectsData.js
@@ -1,4 +1,11 @@
-
+import cardapsImg1 from "../../assets/cardaps1.png";
+import cardapsImg2 from "../../assets/cardaps2.png";
+import portfolioImg1 from "../../assets/portfolio1.png";
+import portfolioImg2 from "../../assets/portfolio2.png";
+import wordpresImg1 from "../../assets/siteWordpress1.png";
+import wordpresImg2 from "../../assets/siteWordpress2.png";
+import volmedImg1 from "../../assets/vollmed1.png";
+import volmedImg2 from "../../assets/vollmed2.png";
 
 // Data for projects cards
 const projectsData = [
@@ -17,17 +24,14 @@ const projectsData = [
         "Spring",
         "PostgreSQL",
       ],
-      imgUrl: [
-        "https://i.ibb.co/kQG3QdX/cardaps.png",
-        "https://i.ibb.co/51Wws3d/cardaps-jpg.png"
-      ],
-      repoUrl: "https://github.com/bBravee/weather-app-react",
+      imgUrl: [cardapsImg1, cardapsImg2],
+      repoUrl: "https://github.com/adriannylelis/api-cardaps",
       height: 300
     },
   ],
   [
     {
-      id: 2,
+      id: 1,
       title: "Voll.Med API",
       shortDescription: "API de uma clinica com funcionalidades de cadastro de médicos, pacientes e agendamentos.",
       description: "Funcionalidades da API: Cadastro de Médicos, listagem de Médicos, atualização de Médicos, exclusão de Médicos, cadastro de Pacientes, listagem de Pacientes, agendamentos de consultas.",
@@ -37,12 +41,31 @@ const projectsData = [
         "Spring",
         "PostgreSQL",
       ],
-      imgUrl: [
-        "https://i.ibb.co/N9K3HbJ/voll-med-med.png",
-        "https://i.ibb.co/mvrYJ9j/voll-med-pac.png"
-      ],
+      imgUrl: [volmedImg1, volmedImg2],
       repoUrl: "https://github.com/adriannylelis/api-voll-med",
       height: 300
+    },
+    {
+      id: 2,
+      title: "Site WordPress",
+      shortDescription: "Site institucional para candidata política.",
+      description:
+        "Desenvolvimento de um site institucional simples e responsivo para uma candidata política, utilizando WordPress para fácil gerenciamento de conteúdo.",
+      badges: ["WordPress", "Design Responsivo"],
+      imgUrl: [wordpresImg1, wordpresImg2],
+      repoUrl: "http://carlarodriguesmanadoceu.com.br",
+      height: 300,
+    },
+    {
+      id: 3,
+      title: "Website Pessoal",
+      shortDescription: "Portfólio pessoal desenvolvido em React e hospedado na Google Cloud.",
+      description:
+        "Website para apresentação de projetos e portfólio profissional, desenvolvido em React e hospedado na Google Cloud Platform. O projeto utiliza Docker para containerização, GitHub Actions para CI/CD e App Engine para hospedagem escalável.",
+      badges: ["JavaScript", "React", "Docker", "GCP", "GitHub Actions"],
+      imgUrl: [portfolioImg1, portfolioImg2],
+      repoUrl: "https://github.com/adriannylelis/personal-portfolio",
+      height: 300,
     },
   ],
 ];
